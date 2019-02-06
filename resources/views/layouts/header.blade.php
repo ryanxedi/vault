3007d6345bb62d16d73b3de98fc712ff6789fd85
XEDI-RYAN\Ryan <ryan@xedi.com>
2019-02-06 20:27:35 +0000

    Added a favicon and updated the title

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,11 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Vault Storage</title>
+        <title>HashiCorp Vault Interface</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossorigin="anonymous">
+
+        <link rel="icon" type="image/png" href="/images/favicon.png" />
+
         <!-- Styles -->
         <style>
             html, body {
